943ab7e13357dc69589a011ac9627125f6ce07bd
Parveen Kumar <parveen.kumar6@gmail.com>
2022-02-25 02:00:07 +0530

    Initial Commit

---
+++ CRYPTOGRAPHY/Encryption/EncryptDecrypt.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace CRYPTOGRAPGY.Encryption
+{
+    public class EncryptDecrypt
+    {
+        private string _data;
+
+        public string Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+
+        private string _key;
+
+        public string Key
+        {
+            get { return _key; }
+            set { _key = value; }
+        }
+
+        public string encrypt()
+        {
+            byte[] bytData;
+            byte[] bytKey;
+            byte[] bytEncoded;
+            byte[] bytIV = { 121, 241, 10, 1, 132, 74, 11, 39, 255, 91, 45, 78, 14, 211, 22, 62 };
+            int iKeyLength;
+            int iKeyRemaining;
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream;
+            RijndaelManaged rijndaelManaged;
+
+            bytData = Encoding.ASCII.GetBytes(_data.ToCharArray());
+            iKeyLength = _key.Length;
+            if (iKeyLength >= 32)
+            {
+                _key = _key.Substring(0, 32);
+            }
+            else
+            {
+                iKeyRemaining = 32 - iKeyLength;
+                _key = _key + new string('X', iKeyRemaining);
+            }
+
+            bytKey = Encoding.ASCII.GetBytes(_key.ToCharArray());
+
+            rijndaelManaged = new RijndaelManaged();
+            cryptoStream = new CryptoStream(memoryStream, rijndaelManaged.CreateEncryptor(bytKey, bytIV), CryptoStreamMode.Write);
+            cryptoStream.Write(bytData, 0, bytData.Length);
+            try
+            {
+                cryptoStream.FlushFinalBlock();
+            }
+            catch { }
+
+            bytEncoded = memoryStream.ToArray();
+
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Convert.ToBase64String(bytEncoded);
+        }
+
+        public string decrypt()
+        {
+            byte[] bytData;
+            byte[] bytKey;
+            //byte[] bytEncoded;
+            byte[] bytIV = { 121, 241, 10, 1, 132, 74, 11, 39, 255, 91, 45, 78, 14, 211, 22, 62 };
+            int iKeyLength;
+            int iKeyRemaining;
+
+            CryptoStream cryptoStream;
+            RijndaelManaged rijndaelManaged;
+
+            //bytData = Encoding.ASCII.GetBytes(_data.ToCharArray());
+            bytData = Convert.FromBase64String(_data);
+
+            iKeyLength = _key.Length;
+            if (iKeyLength >= 32)
+            {
+                _key = _key.Substring(0, 32);
+            }
+            else
+            {
+                iKeyRemaining = 32 - iKeyLength;
+                _key = _key + new string('X', iKeyRemaining);
+            }
+
+            bytKey = Encoding.ASCII.GetBytes(_key.ToCharArray());
+
+            byte[] bytTemp = new byte[bytData.Length];
+            MemoryStream memoryStream = new MemoryStream(bytData);
+            rijndaelManaged = new RijndaelManaged();
+            cryptoStream = new CryptoStream(memoryStream, rijndaelManaged.CreateDecryptor(bytKey, bytIV), CryptoStreamMode.Read);
+            cryptoStream.Read(bytTemp, 0, bytTemp.Length);
+
+            try
+            {
+                cryptoStream.FlushFinalBlock();
+            }
+            catch { }
+
+            //bytEncoded = memoryStream.ToArray();
+
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Encoding.ASCII.GetString(bytTemp);
+        }
+    }
+}
+  
---
+++ CRYPTOGRAPHY/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+string encrypt;
+string decrypt;
+CRYPTOGRAPGY.Encryption.EncryptDecrypt objEncrypt = new CRYPTOGRAPGY.Encryption.EncryptDecrypt();
+CRYPTOGRAPGY.Encryption.EncryptDecrypt objDecrypt = new CRYPTOGRAPGY.Encryption.EncryptDecrypt();
+objEncrypt.Key = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+objEncrypt.Data = "Parveen";
+
+Console.WriteLine("Data is : " + "Parveen");
+encrypt = objEncrypt.encrypt();
+
+Console.WriteLine("Encrypted Data : " + encrypt);
+objDecrypt.Key = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+objDecrypt.Data = encrypt;
+decrypt = objDecrypt.decrypt();
+
+Console.WriteLine("Decrypted Data : " + decrypt);
+
+
+
